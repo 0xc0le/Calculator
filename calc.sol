2df605fbeaa52f8eb4a2b24273fde6af0153c273
0xc0le <cole511@protonmail.com>
2022-04-05 21:04:46 -0400

    formatting

--- calc.sol
+++ calc.sol
@@ -5,26 +5,26 @@ contract Calculator {
     int private lastvalue = 0;
 
     function Add(int a, int b) public returns (int) {
-    lastvalue = a + b;
-    return lastvalue;
+        lastvalue = a + b;
+        return lastvalue;
     }
     
     function Subtract(int a, int b) public returns (int) {
-    lastvalue = a - b;
-    return lastvalue;
+        lastvalue = a - b;
+        return lastvalue;
     }
 
     function Multiply(int a, int b) public returns (int){
-    lastvalue = a * b;
-    return lastvalue;
+        lastvalue = a * b;
+        return lastvalue;
     }
 
     function Divide(int a, int b) public returns (int){
-    lastvalue = a / b;
-    return lastvalue;
+        lastvalue = a / b;
+        return lastvalue;
     }
 
     function LastOperation() public constant returns (int) {
-    return lastvalue;
+        return lastvalue;
     }
 }
